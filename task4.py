1ba87560d70938490fb2e2d1fe626a1f4fcbef02
lexskop <lexskop@gmail.com>
2022-12-16 20:29:09 +0700

    решена пятая задачка

--- task4.py
+++ task4.py
@@ -3,4 +3,4 @@
 URL_list = ['https://city.Hello-world.com/hello/','https://Привет.рф/Приветствие/','https://www.MPCS.ru/','https://Zaday-vopros.su/vopros/kak-dela/']
 result = list(map(lambda x: URL_list[x].partition('//')[-1],[x for x in range(len(URL_list))]))
 result = list(map(lambda x: result[x].partition('/')[0],[x for x in range(len(result))]))
-print(result)
+print(f'Исходный список {URL_list} ; результат -> {result}')
