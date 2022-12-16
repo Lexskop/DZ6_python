b9c95bf1781c342db9bfbb052be5898a500ca6f6
lexskop <lexskop@gmail.com>
2022-12-16 20:13:41 +0700

    решена четвертая задачка

--- task4.py
+++ task4.py
@@ -1,1 +1,6 @@
 # 4 - Дан список URL различных сайтов. Нужно составить список доменных имен сайтов.
+
+URL_list = ['https://city.Hello-world.com/hello/','https://Привет.рф/Приветствие/','https://www.MPCS.ru/','https://Zaday-vopros.su/vopros/kak-dela/']
+result = list(map(lambda x: URL_list[x].partition('//')[-1],[x for x in range(len(URL_list))]))
+result = list(map(lambda x: result[x].partition('/')[0],[x for x in range(len(result))]))
+print(result)
