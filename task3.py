abbdb2a4abb79f9764580ddad8ef63b4d2dc248e
lexskop <lexskop@gmail.com>
2022-12-16 19:51:38 +0700

    решена третья задачка

--- task3.py
+++ task3.py
@@ -1,2 +1,6 @@
 # 3-Сформировать список из N членов последовательности.
 # Для N = 5: 1, -3, 9, -27, 81 и т.д.
+
+number_to_work = int(input('Введите число N -> '))
+result = list(map(lambda x: (-3)**x, [x for x in range(number_to_work)]))
+print(f'Ваше число -> {number_to_work}, Результат -> {result}')
